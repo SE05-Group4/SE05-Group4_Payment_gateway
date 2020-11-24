1ed4642e25731ca4f07000b54e166dd9596c7576
Tuyen <phamanhtuyen2000@gmail.com>
2020-11-24 17:21:56 +0700

    tuyen1

---
+++ frontend/config.php
@@ -0,0 +1,11 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+$vnp_TmnCode = "7HXB8EFD"; //Mã website tại VNPAY 
+$vnp_HashSecret = "VFPLHNNZPTWBZOMVYKETNHMFRUBPULXX"; //Chuỗi bí mật
+$vnp_Url = "http://sandbox.vnpayment.vn/paymentv2/vpcpay.html";
+$vnp_Returnurl = "http://localhost/Payment-gateway/frontend/vnpay_return.php";
